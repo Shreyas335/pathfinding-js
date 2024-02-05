719a64875cf999f0ef2ddf117fe3202227d36406
aghosh376 <arayboghosh@gmail.com>
2024-02-05 13:38:21 -0800

    Added wall color

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -4,28 +4,35 @@ import React, { useState } from 'react';
 import './Grid.css'; // Import the CSS file
 
 const Grid = ({ rows, columns }) => {
-    const [grid, setGrid] = useState(Array.from({ length: rows }, () => Array(columns).fill(false)));
+    const [grid, setGrid] = useState(Array.from({ length: rows }, () => Array(columns).fill(0)));
     const [currentColor, setCurrentColor] = useState('#00f');
 
-    const getColor = () => {
-        return '#000000';
+    const getColor = (color) => {
+        if (color == 0) {
+            return '#000000';
+        } else if (color === 1) {
+            return '#fc0000';
+        } else if (color === 2){
+            return '#eeeeee'
+        }
     };
 
     const toggleCell = (row, col) => {
         const newGrid = [...grid];
-        newGrid[row][col] = !newGrid[row][col];
+        newGrid[row][col]++;
+        if (newGrid[row][col] == 3) {newGrid[row][col] = 0}
         setGrid(newGrid);
 
         setCurrentColor(getColor());
     };
-
+    
     return (
         <div className="grid-container" style={{ gridTemplateColumns: `repeat(${columns}, 14px)` }}>
             {grid.map((row, rowIndex) =>
                 row.map((cell, colIndex) => (
                     <div
                         key={`${rowIndex}-${colIndex}`}
-                        className={`grid-cell ${grid[rowIndex][colIndex] ? 'active-cell' : ''}`}
+                        className={`grid-cell ${grid[rowIndex][colIndex] == 1  ? 'active-cell' : grid[rowIndex][colIndex] == 2 ? 'wall-cell' : ''}`}
                         onClick={() => toggleCell(rowIndex, colIndex)}
                     />
                 ))
