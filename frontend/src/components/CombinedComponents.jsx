c7436c5efade482d826d96208fc3b316563606a0
Shreyas335 <shreyasgosakan@gmail.com>
2024-07-03 22:14:16 -0400

    django api first version not working

--- frontend/src/components/CombinedComponents.jsx
+++ frontend/src/components/CombinedComponents.jsx
@@ -96,41 +96,7 @@ const Controls = () => {
   const gridRef = useRef(null); // Create a ref to hold the Grid component instance
   const [selectedMaze, setSelectedMaze] = useState('');
 
-  useEffect(() => {
-    loadMaze();
-  }, []);
-
-  const saveMaze = async () => {
-    if (gridRef.current) {
-      const gridData = gridRef.current.grid; // Access the grid data if needed
-      try {
-        const response = await fetch('http://127.0.0.1:5000/api/save', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ gridData })
-        });
-        if (!response.ok) {
-          throw new Error('Failed to save data');
-        }
-        console.log('Data saved successfully');
-      } catch (error) {
-        console.error('Error saving data:', error);
-      }
-    }
-  };
-
-  const loadMaze = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:5000/api/load');
-      const data = await response.json();
-      if (gridRef.current) {
-        gridRef.current.setGrid(data.grid); // Set grid data through ref
-        console.log('Grid loaded successfully');
-      }
-    } catch (error) {
-      console.error('Error loading grid:', error);
-    }
-  };
+  
 
   const selectMaze = (event) => {
     setSelectedMaze(event.target.value);
